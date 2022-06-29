12a99efea2f015dc12543279e09b2ef56d153daa
anuragsingh23 <anuragshekhawat17@gmail.com>
2022-06-30 01:34:16 +0530

    Order UI design complete

--- app/src/main/java/com/example/coffeeshop/ui/orderscreen/OrderViewModel.kt
+++ app/src/main/java/com/example/coffeeshop/ui/orderscreen/OrderViewModel.kt
@@ -1,4 +1,24 @@
 package com.example.coffeeshop.ui.orderscreen
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 class OrderViewModel {
+
+    private val _score = MutableLiveData<Int>()
+
+    val score : LiveData<Int> = _score
+
+    init {
+        _score.value = 0
+    }
+
+    fun increment(){
+
+        _score.value = score.value?.plus(1)
+    }
+
+    fun reset(){
+        _score.value = 0
+    }
 }
